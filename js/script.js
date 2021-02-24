7d0d849aee6ba5da918f956433a8c59450c97a3b
Vse Putyom <bertraid777@gmail.com>
2021-02-24 22:22:14 +0300

    fix(script.js) timer

--- js/script.js
+++ js/script.js
@@ -24,27 +24,29 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         let timer = getTimeRemaining();
 
-        let idInterval = setInterval(function () {
-            updateClock();
-        }, 1000);
-
-        setTimeout(function () {
-            clearInterval(idInterval);
-        }, timer.timeRemaining * 1000);
-
         function updateClock() {
             let timer = getTimeRemaining();
-            if(timer.hours < 10 && timer.hours >= 0){
+            if (timer.hours < 10 && timer.hours >= 0) {
                 timer.hours = '0' + timer.hours;
-            }if(timer.minutes < 10 && timer.minutes >= 0){
-                timer.minutes = '0' + timer.minutes
-            }if(timer.seconds < 10 && timer.seconds >= 0){
-                timer.seconds = '0' + timer.seconds
+            }
+            if (timer.minutes < 10 && timer.minutes >= 0) {
+                timer.minutes = '0' + timer.minutes;
+            }
+            if (timer.seconds < 10 && timer.seconds >= 0) {
+                timer.seconds = '0' + timer.seconds;
             }
             timerHours.textContent = timer.hours;
             timerMinutes.textContent = timer.minutes;
             timerSeconds.textContent = timer.seconds;
+
+            setTimeout(function () {
+                clearInterval(idInterval);
+            }, timer.timeRemaining * 1000);
         }
+
+        let idInterval = setInterval(function () {
+            updateClock();
+        }, 1000);
     }
 
     countTimer('25 february 2021 14:48:00');
